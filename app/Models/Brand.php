625f75276927b8f767a05219445b5d8cf135b96f
Tanvirul Islam <ownbinodon@gmail.com>
2025-07-29 13:45:33 +0600

    fix the problem in brands, updated the table and protection in model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    protected $fillable = ['brandName', 'brandImg'];
+    protected $guarded = [];
 
     public function products()
     {
